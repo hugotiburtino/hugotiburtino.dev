fc16b3b5da38ce212ab8403c14d9d6bd92cab1bd
Hugo B Tiburtino <hugotiburtino@protonmail.com>
2020-06-29 21:35:36 +0200

    Get rid of magic nums, place the commentary at right place
    and delete forgotten console.log

--- src/app/projects-panel/projects-panel.component.ts
+++ src/app/projects-panel/projects-panel.component.ts
@@ -35,19 +35,20 @@ export class ProjectsPanelComponent implements OnInit {
   /**
    * Handles the action button clicks of a project card
    * @param target The button clicked
-   * Note: since Angular Material wraps text of the button element
-   * into a node by itself, we need to find the value of the button first
-   * See const value below
    * @param project The project whose card button was clicked
    */
   onClick(target, project) {
-    const value: number = target.value | target.parentElement.value
-    console.log(value)
-    if (value === 0) {
+    /**
+     * Since Angular Material wraps text of the button element
+     * into a node by itself, we need to find the value of the button first
+     */
+    const value: number = target.value | target.parentElement.value;
+    const [SEE, INFO, SOURCE_CODE] = [0, 1, 2];
+    if (value === SEE) {
       window.open(project.url, '_blank').focus();
-    } else if (value === 1) {
+    } else if (value === INFO) {
       // Implementme
-    } else if (value === 2) {
+    } else if (value === SOURCE_CODE) {
       window.open(project.srcCodeUrl, '_blank').focus();
     }
   }
