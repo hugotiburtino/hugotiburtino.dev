fdc2a755e24a210c66c65396a345783bdde6fb76
Hugo B Tiburtino <hugotiburtino@protonmail.com>
2020-07-04 22:41:50 +0200

    Create contact component
    It may be used later to have an messaging feature.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HeaderComponent } from './header/header.component';
 import { IdRefPipe } from './lib/idref.pipe';
 import { AboutmeComponent } from './aboutme/aboutme.component';
 import { SkillsPanelComponent } from './skills-panel/skills-panel.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @NgModule({
@@ -25,7 +26,8 @@ import { SkillsPanelComponent } from './skills-panel/skills-panel.component';
     HeaderComponent,
     IdRefPipe,
     AboutmeComponent,
-    SkillsPanelComponent
+    SkillsPanelComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
